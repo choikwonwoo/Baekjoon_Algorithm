6878d76f6f3558020246712abbaf53e18b6bfbb8
choi kwon woo <65008329+choikwonwoo@users.noreply.github.com>
2023-11-01 21:32:57 +0900

    [Bronze V] Title: 사칙연산, Time: 132 ms, Memory: 14424 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[]args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] list = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        System.out.println(list[0]+list[1]);
+        System.out.println(list[0]-list[1]);
+        System.out.println(list[0]*list[1]);
+        System.out.println(list[0]/list[1]);
+        System.out.println(list[0]%list[1]);
+    }
+}
