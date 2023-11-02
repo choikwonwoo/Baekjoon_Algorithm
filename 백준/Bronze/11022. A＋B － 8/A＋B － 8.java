d9d06bf9a7c5513e528aca4d4fae13c8c864ebde
choi kwon woo <65008329+choikwonwoo@users.noreply.github.com>
2023-11-02 21:59:41 +0900

    [Bronze V] Title: A+B - 8, Time: 188 ms, Memory: 16696 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[]args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++){
+            int[] list = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            sb.append("Case #"+i+": " + list[0] +" + "+ list[1] +" = "+ (list[0]+list[1]) + "\n");
+        }
+
+        System.out.println(sb);
+    }
+}
