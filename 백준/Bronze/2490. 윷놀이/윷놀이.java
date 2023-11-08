3e36569dfe4298a69b1c5e1c958466ead5dfbb90
choi kwon woo <65008329+choikwonwoo@users.noreply.github.com>
2023-11-08 15:32:42 +0900

    [Bronze III] Title: 윷놀이, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/2490. 윷놀이/윷놀이.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 3; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int yut = 0;
+            while (st.hasMoreTokens()){
+                yut += Integer.parseInt(st.nextToken());
+            }
+            switch (yut){
+                case 0:
+                    System.out.println("D");
+                    break;
+                case 1:
+                    System.out.println("C");
+                    break;
+                case 2:
+                    System.out.println("B");
+                    break;
+                case 3:
+                    System.out.println("A");
+                    break;
+                case 4:
+                    System.out.println("E");
+                    break;
+            }
+        }
+    }
+}
