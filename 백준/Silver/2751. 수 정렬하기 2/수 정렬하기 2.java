4a20004c95bb3f27cb05441b8a6322c20b325546
choi kwon woo <65008329+choikwonwoo@users.noreply.github.com>
2023-11-25 19:53:17 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 964 ms, Memory: 137592 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,21 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb  = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        int[] list = new int[n];
+        int[] list = new int[1000001];
+        int[] list1 = new int[1000001];
+
         for (int i = 0; i < n; i++){
             int num = Integer.parseInt(br.readLine());
-            list[i] = num;
+            if (num > 0){
+                list[num-1] = 1;
+            } else if (num < 0){
+                list1[(num*-1)] = 1;
+            }else{
+                list1[0] = 1;
+            }
         }
-        list = Arrays.stream(list).sorted().toArray();
+        for (int i = list1.length-1; i >= 0; i--){
+            if (list1[i] == 1){
+                sb.append(i*-1 + "\n");
+            }
+        }
         for (int i = 0; i < list.length; i++){
-            sb.append(list[i] + "\n");
+            if (list[i] == 1){
+                sb.append(i+1 + "\n");
+            }
         }
         System.out.println(sb);
     }
