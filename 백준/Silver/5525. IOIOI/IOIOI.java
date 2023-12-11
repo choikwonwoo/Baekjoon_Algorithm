9bf91c21d886da24a8e9a6617a47cf3501161a7d
choi kwon woo <65008329+choikwonwoo@users.noreply.github.com>
2023-12-11 22:44:19 +0900

    [Silver I] Title: IOIOI, Time: 272 ms, Memory: 22044 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,32 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb1 = new StringBuilder("I");
-        StringBuilder sb2 = new StringBuilder();
 
-        int n = Integer.parseInt(br.readLine());
-        int length = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        char s[] = br.readLine().toCharArray();
+
+        int result = 0;
         int count = 0;
-        sb2.append(br.readLine());
 
-        for (int i = 0; i < n; i++){
-            sb1.append("OI");
-        }
-
-        int sb1Length = sb1.length();
-
-        for (int i = 0; i < length - sb1Length + 1; i++){
-            String sub = sb2.substring(i, i + sb1Length);
-            if (sub.equals(sb1.toString())){
+        for(int i=1; i < M - 1; i++) {
+            if(s[i - 1] == 'I' && s[i] == 'O' && s[i + 1] == 'I') {
                 count++;
+
+                if(count == N) {
+                    count--;
+                    result++;
+                }
                 i++;
+            } else {
+                count = 0;
             }
         }
-        System.out.println(count);
-    }
-}
+
+        System.out.println(result);
+
+    } // End of main
+} // End of class
